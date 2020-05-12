658cf869945c1a3adae4a4f1b07915ab8ef0db9c
ash-511 <aishwarya51100@gmail.com>
2020-05-12 14:34:35 +0530

    Add about text

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const partialsPath = path.join(__dirname, '../templates/partials')
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+hbs.registerPart ials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Aiswarya Suresh'
     })
 })
 
